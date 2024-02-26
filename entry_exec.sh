0119f4e4dc80a231af4a2998befaafe0a908d587
ASAKA-219 <118964999+ASAKA-219@users.noreply.github.com>
2024-02-27 01:02:22 +0900

    Update entry_exec.sh

--- entry_exec.sh
+++ entry_exec.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker compose up -d --build turtlebot3_noetic ;docker compose exec turtlebot3_noetic /bin/bash
+container_name=$(docker ps --format ',{{.Names}}')
+if [ "$container_name == noetic-tb3" ] ;then
+  echo $container_name 'is already running.'
+  docker compose up -d
+else
+  docker compose up -d --build
+fi
+docker compose exec turtlebot3_noetic /bin/bash
